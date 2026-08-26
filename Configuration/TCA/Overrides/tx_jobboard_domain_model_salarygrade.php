770e7e80e1f221408eaadb4ea78d75a261d7d6cb
Stefan Froemken <sfroemken@jweiland.net>
2026-08-26 10:18:33 +0200

    [TASK] Fix Build/cgl/config.php copied from another extension
    The ruleset had been copied over from an older extension and was
    missing the jweiland.net-specific overrides layer (braces_position,
    operator_linebreak, method_chaining_indentation and a handful of
    others), plus it still used the deprecated "@PER-CS1.0" set name and
    a "php_unit_test_case_static_method_calls" configuration without the
    per-method overrides that keep mock builder calls like
    "->expects($this->once())" on "$this->" while assertions stay on
    "self::".
    
    Reapplying "-s cgl" with the corrected ruleset only changed operator
    placement in two TCA/Overrides files - no functional difference.

--- Configuration/TCA/Overrides/tx_jobboard_domain_model_salarygrade.php
+++ Configuration/TCA/Overrides/tx_jobboard_domain_model_salarygrade.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the package jweiland/jobboard.
  *
@@ -11,10 +13,10 @@ if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
-$GLOBALS['TCA']['tx_jobboard_domain_model_salarygrade']['columns']['starttime']['description'] =
-    'LLL:EXT:jobboard/Resources/Private/Language/locallang_db.xlf:tx_jobboard_domain_model_salarygrade.starttime.description';
+$GLOBALS['TCA']['tx_jobboard_domain_model_salarygrade']['columns']['starttime']['description']
+    = 'LLL:EXT:jobboard/Resources/Private/Language/locallang_db.xlf:tx_jobboard_domain_model_salarygrade.starttime.description';
 $GLOBALS['TCA']['tx_jobboard_domain_model_salarygrade']['columns']['starttime']['config']['behaviour']['allowLanguageSynchronization'] = true;
 
-$GLOBALS['TCA']['tx_jobboard_domain_model_salarygrade']['columns']['endtime']['description'] =
-    'LLL:EXT:jobboard/Resources/Private/Language/locallang_db.xlf:tx_jobboard_domain_model_salarygrade.endtime.description';
+$GLOBALS['TCA']['tx_jobboard_domain_model_salarygrade']['columns']['endtime']['description']
+    = 'LLL:EXT:jobboard/Resources/Private/Language/locallang_db.xlf:tx_jobboard_domain_model_salarygrade.endtime.description';
 $GLOBALS['TCA']['tx_jobboard_domain_model_salarygrade']['columns']['endtime']['config']['behaviour']['allowLanguageSynchronization'] = true;
--- Configuration/TCA/Overrides/tx_jobboard_domain_model_salarytable.php
+++ Configuration/TCA/Overrides/tx_jobboard_domain_model_salarytable.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the package jweiland/jobboard.
  *
@@ -11,10 +13,10 @@ if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
-$GLOBALS['TCA']['tx_jobboard_domain_model_salarytable']['columns']['starttime']['description'] =
-    'LLL:EXT:jobboard/Resources/Private/Language/locallang_db.xlf:tx_jobboard_domain_model_salarytable.starttime.description';
+$GLOBALS['TCA']['tx_jobboard_domain_model_salarytable']['columns']['starttime']['description']
+    = 'LLL:EXT:jobboard/Resources/Private/Language/locallang_db.xlf:tx_jobboard_domain_model_salarytable.starttime.description';
 $GLOBALS['TCA']['tx_jobboard_domain_model_salarytable']['columns']['starttime']['config']['behaviour']['allowLanguageSynchronization'] = true;
 
-$GLOBALS['TCA']['tx_jobboard_domain_model_salarytable']['columns']['endtime']['description'] =
-    'LLL:EXT:jobboard/Resources/Private/Language/locallang_db.xlf:tx_jobboard_domain_model_salarytable.endtime.description';
+$GLOBALS['TCA']['tx_jobboard_domain_model_salarytable']['columns']['endtime']['description']
+    = 'LLL:EXT:jobboard/Resources/Private/Language/locallang_db.xlf:tx_jobboard_domain_model_salarytable.endtime.description';
 $GLOBALS['TCA']['tx_jobboard_domain_model_salarytable']['columns']['endtime']['config']['behaviour']['allowLanguageSynchronization'] = true;
